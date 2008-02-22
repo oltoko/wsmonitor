d24f5de0f00dc1ee48d18e22ca55733e79208550
arungupta <arungupta@5a2a8073-865c-4611-bf4c-e4f645e35407>
2008-02-22 18:50:53 +0000

    zone file
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:

---
+++ www/zone-tools.js
@@ -0,0 +1,27 @@
+var zones = [
+	{
+		title: "Hudson",
+		href:  "https://hudson.dev.java.net/"
+	},
+	{
+		title: "Japex",
+		href:  "https://japex.dev.java.net/"
+	},
+	{
+		title: "openInstaller",
+		href:  "https://openinstaller.dev.java.net/"
+	},
+    {
+        title: "IDE Plugins",
+        href:  "https://glassfishplugins.dev.java.net/"
+    },
+    {
+        title: "WSMonitor",
+        href: "https://wsmonitor.dev.java.net/"
+    },
+    {
+        title: "Governance Document",
+        href:  "https://glassfish.dev.java.net/public/GovernancePolicy.html"
+    }
+];
+
